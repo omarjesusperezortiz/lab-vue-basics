56e922efbe5ba312f71de6b5809ab41423465dcb
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-10-11 21:38:31 +0200

    done

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,15 @@
 <template>
+<div :class="[clase]">
+  <input @click="cambioBackground" type="button" value="presiona este boton">
   <NavBar />
-  <Suma />
-  <Loop />
-  <Boolean />
-  <Footer />
+    <Suma />
+    <Loop />
+    <Boolean />
+    <Footer />
+</div>
+
+
+
 </template>
 <script setup>
 
@@ -12,8 +18,26 @@ import Boolean from "./components/Boolean.vue"
 import Suma from "./components/Suma.vue"
 import Footer from "./components/Footer.vue"
 import Loop from "./components/Loop.vue"
+import { ref } from 'vue'
+
+
+
+const clase = ref("white")
+function cambioBackground() { 
+  console.log(clase.value)
+  clase.value= "black"}
 
 </script>
 
+
+
 <style scoped>
+
+.white{
+  background-color: white;
+}
+
+.black{
+  background-color: black;
+}
 </style>

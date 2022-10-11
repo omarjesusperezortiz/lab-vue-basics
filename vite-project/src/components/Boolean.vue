84c327e3dafbee20263bf7b0aa0c700be5a01856
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-10-11 16:47:28 +0200

    changes

---
+++ vite-project/src/components/Boolean.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="section">
+        <button @click="awesome = !awesome">Toggle</button>
+        <h1 v-if="awesome">Vue is awesome!</h1>
+        <h1 v-else>Oh no 😢</h1>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const awesome = ref(true);
+
+
+</script>
+<style scoped>
+</style>
---
+++ vite-project/src/components/Suma.vue
@@ -0,0 +1,26 @@
+<template>
+
+<div class="section">
+<p>El resultado de 2+2 es: {{operation}}</p>
+
+<p>El resultado de 2+2 es: {{ suma }}</p>
+
+<button @click="sumatoria">Muestra el resultado de 2+2 </button>
+</div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+const operation = 2+2
+
+const suma = ref("...")
+
+
+
+function sumatoria() { suma.value = 4}
+
+</script>
+
+<style scoped>
+</style>

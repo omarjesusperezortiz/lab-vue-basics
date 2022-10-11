c3ad80f71efeaef31247a917f9562cab39b47370
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-10-11 17:27:58 +0200

    final

---
+++ vite-project/src/components/Loop.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div class="section">
+        
+        <li v-for="(value, key) in myObject">
+        {{ key }}: {{ value }}
+        </li>
+            </div>
+
+</template>
+<script >
+export default {
+data() {
+  return {
+    myObject: {
+      title: 'How to do lists in Vue',
+      Description: 'Dudado',
+      Content: 'Lorem ipsum Loremp loremroamsdomerapmeaprmpae'
+    }
+  }
+}
+}
+</script>
+<style scoped>
+</style>
